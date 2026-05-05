2134a6671bfa040c56a7e4ba41bbffa2342ec6b9
Lipsita Khadgarai <lipsitakhadgarai16@gmail.com>
2026-05-05 13:21:12 +0530

    5/5/26 i have created dsa java repo

--- src/dsaquestions/fibbinacii.java
+++ src/dsaquestions/fibbinacii.java
@@ -1,4 +1,19 @@
 package dsaquestions;
 
+import java.util.Scanner;
+
 public class fibbinacii {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a nth term of fibinacci series you want to know");
+        int n = sc.nextInt();
+        int a = 0,b = 1,c = 0;
+        for(int i = 0;i<n;i++){
+            System.out.println(a);
+            c = a+b;
+            a = b;
+            b = c;
+        }
+        System.out.println("The "+n+"th fibonacci number is : "+a);
+    }
 }
