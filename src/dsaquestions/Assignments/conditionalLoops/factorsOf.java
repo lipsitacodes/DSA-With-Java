b4161f2a53cfce26927e690d0f6c705276023e7e
Lipsita Khadgarai <lipsitakhadgarai16@gmail.com>
2026-05-25 20:08:02 +0530

    25-05-26

---
+++ src/dsaquestions/Assignments/conditionalLoops/factorsOf.java
@@ -0,0 +1,17 @@
+package dsaquestions.Assignments.conditionalLoops;
+
+import java.util.Scanner;
+
+class factorsOfAnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+        System.out.println("Factors of "+n+" are : ");
+        for (int i = 1;i<=n;i++){
+            if(n%i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
