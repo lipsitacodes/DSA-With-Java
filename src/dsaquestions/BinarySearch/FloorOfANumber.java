7a8198a1ce5f8e439e0b6c66a0d171e077824a4d
Lipsita <lipsitakhadgarai16@gmail.com>
2026-05-31 16:47:42 +0530

    31/05/26 Floor of number problem is copmleted

--- src/dsaquestions/BinarySearch/FloorOfANumber.java
+++ src/dsaquestions/BinarySearch/FloorOfANumber.java
@@ -1,8 +1,8 @@
 package dsaquestions.BinarySearch;
 
 import java.util.Scanner;
-//find the smallest number which is greater than equal to the target number smallest upper bound
-public class CeilingOfNumber {
+//find the largest number which is smaller than or equal to the target number largest lower bound
+public class FloorOfANumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of the array");
@@ -15,7 +15,7 @@ public class CeilingOfNumber {
         System.out.println("Enter Your Target Number : ");
         int t = sc.nextInt();
         int c = BSC(a, t);
-        System.out.println("Ceiling Number of" + t + " is : " + c);
+        System.out.println("Floor Number of " + t + " is : " + c);
     }
 
     static int BSC(int[] a, int t) {
@@ -32,6 +32,17 @@ public class CeilingOfNumber {
                 h = mid - 1;
             }
         }
-        return a[l];
+        return a[h];
     }
-}
+    }
+
+//[12,23,24,56,75,77,78,89]
+// 0  1  2  3  4  5  6  7
+// now her our target is basically target = 45 l = 0 , h = 7 , m = 3
+// 45 <= 56[mid]
+//l = 0,h = mid - 1 = 3-1 = 2
+//[12,23,24] m = 1
+// 0  1  2
+//23 < = 43 l = m+1 = 2, h = 2
+// [24] index = 2 , l = 2, h = 2 is our target element is smaller or greater than the mid value
+// 45 >= 24 l = mid+1 = 2+1 = 3 ...this is the condition where our while loop exits and return the h over
