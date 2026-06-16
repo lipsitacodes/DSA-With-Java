916a0f43c4f3b9910c0cd65edc441c75fad34bd6
Lipsita <lipsitakhadgarai16@gmail.com>
2026-06-16 19:18:43 +0530

    16/06/26 Leetcode problem number 34 solved

--- src/dsaquestions/BinarySearch/First_Last_Element.java
+++ src/dsaquestions/BinarySearch/First_Last_Element.java
@@ -1,33 +1,43 @@
-//package dsaquestions.BinarySearch;
-//
-//import java.util.Arrays;
-//import java.util.Scanner;
-//
-//public class First_Last_Element {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter size of the array");
-//        int n = sc.nextInt();
-//        int[] arr = new int[n];
-//        System.out.println("Enter element into the array");
-//        for (int i = 0; i < n; i++) {
-//            arr[i] = sc.nextInt();
-//        }
-//        System.out.println("Enter your target element");
-//        int t = sc.nextInt();
-//        System.out.println(Arrays.toString(fle(arr,t)));
-//    }
-//
-// static int[] fle(int[] arr, int t) {
-//        int l = 0,i,j;
-//        int h = arr.length- 1;
-//        while(i<= h){
-//            int mid = l+(h-l)/2;
-//            if(arr[mid] == t){
-//                h = mid -1;
-//
-//            }
-//        }
-//        return [-1,-1];
-//    }
-//}
+package dsaquestions.BinarySearch;
+
+
+import java.util.Arrays;
+//this function just returns the index value of target
+
+public class First_Last_Element {
+    public static void main(String[] args) {
+        int[] arr = {3, 7, 7, 7, 7, 8, 8, 10};
+        int t = 2;
+        System.out.println(Arrays.toString(fle(arr, t)));
+    }
+    static int[] fle(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        //Check for first occurrence if target first
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+    static int search(int[] nums, int target, boolean findstartindex) {
+        int ans = -1;
+        int l = 0;
+        int h = nums.length - 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                if (findstartindex) {
+                    h = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
